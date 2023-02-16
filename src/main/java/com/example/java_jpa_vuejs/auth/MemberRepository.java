9a3fb2c889266c8549ffa832c9f6bcc1117877bb
cho younghyun <5869jyh@hanmail.net>
2023-02-16 17:46:36 +0900

    Work_소스정리

--- src/main/java/com/example/java_jpa_vuejs/auth/MemberRepository.java
+++ src/main/java/com/example/java_jpa_vuejs/auth/MemberRepository.java
@@ -6,9 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+
 
 import org.springframework.data.repository.CrudRepository;
 
