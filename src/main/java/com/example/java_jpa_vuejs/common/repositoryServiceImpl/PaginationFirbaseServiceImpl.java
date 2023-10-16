699095bbe2aa2d8abaa31997e7493a7ddb80df18
cho younghyun <5869jyh@hanmail.net>
2023-10-16 17:51:30 +0900

    Work_페이징공통 마무리

--- src/main/java/com/example/java_jpa_vuejs/common/repositoryServiceImpl/PaginationFirbaseServiceImpl.java
+++ src/main/java/com/example/java_jpa_vuejs/common/repositoryServiceImpl/PaginationFirbaseServiceImpl.java
@@ -43,7 +43,7 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
     @Override
     public String getDocIdList(PaginationDto paginationDto) throws Exception {
 
-        long reqTime = Util.durationTime ("start", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", 0, "Proceeding ::: " );
+        long reqTime = Util.durationTime ("start", "CLOUD / < GET PAGING DOC LIST - SELECT > : ", 0, "Proceeding ::: " );
 
         LOG.info("도큐먼트ID 리스트 호출 - 기준 CurrentPage : " + paginationDto.getCurrentPage());
 
@@ -204,16 +204,17 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
                 }
 
                 strDocIdList = Arrays.toString(docIdList.toArray()).replaceAll("\\[","").replaceAll("\\]","");; 
+                
                 return strDocIdList;
             }
             else if(!strCallType.equals("EACH")){
                 
                 if((intPageGroupEnd * intCountPerPage) >= intTotalCount){//마지막 페이지 일때
-                    //System.out.println("☆  11  1                ------------------------------------------------------  ☆  = " + intTotalCount);
+                    LOG.info("도큐멘트 리스트 호출 EACH1 - intPageGroupStart : " + intPageGroupStart);
                     strDocIdList = paginationDto.getDocIdArr();
                 }
                 else{
-                    //System.out.println("☆   222                 ------------------------------------------------------  ☆  = " + intTotalCount);
+                    LOG.info("도큐멘트 리스트 호출 EACH2 - intPageGroupStart : " + intPageGroupStart);
                     //햔제 도큐먼트ID를 기준으로 스냅샷 생성
                     ApiFuture<DocumentSnapshot> future = db.collection(strCollectionNm).document(strCurrentDocId).get();
                     DocumentSnapshot snapshot = future.get(30, TimeUnit.SECONDS);
@@ -240,11 +241,9 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
 
                 return strDocIdList;
             }
-           
-            //Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Complete ::: " );
         }
         catch (Exception e) {
-			Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Fail ::: " );
+			Util.durationTime ("end", "CLOUD / < GET PAGING DOC LIST - SELECT > : ", reqTime, "Fail ::: " );
             e.printStackTrace();
         }
 
@@ -261,7 +260,7 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
     @Override
     public String getFirstDoc(PaginationDto paginationDto) throws Exception {
 
-        long reqTime = Util.durationTime ("start", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", 0, "Proceeding ::: " );
+        long reqTime = Util.durationTime ("start", "CLOUD / < GET PAGING FIRST DOC - SELECT > : ", 0, "Proceeding ::: " );
 
         String strFirstDoc = "";//첫번째 도큐먼트ID
         String intCurrentPage = paginationDto.getCurrentPage();//현재 페이지 정보
@@ -286,10 +285,10 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
                 paginationDto.setCurrentPage(strFirstDoc);
             }
             
-            Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Complete ::: " );
+            Util.durationTime ("end", "CLOUD / < GET PAGING FIRST DOC - SELECT > : ", reqTime, "Complete ::: " );
         }
         catch (Exception e) {
-			Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Fail ::: " );
+			Util.durationTime ("end", "CLOUD / < GET PAGING FIRST DOC - SELECT > : ", reqTime, "Fail ::: " );
             e.printStackTrace();
         }
 
@@ -315,8 +314,7 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
         Integer intCountPerPage = paginationDto.getCountPerPage();// 페이지에서 제공할 게시물의 수
 		Integer intPageGroupSize = paginationDto.getBlockPage();// 패아장을 제공할 페이지 그룹의 수
         Integer intTotalCount = paginationDto.getTotalCount();// 총 게시물 갯수
-        
-        Integer intPageTotal = (intTotalCount/ intCountPerPage);// 총 페이지 수
+    
         Integer intPageGroupStart = (((intCurrentPage ) / intPageGroupSize) * intPageGroupSize);//다음 페이지 그룹의 시작점
         Integer intPageGroupEnd = (intPageGroupStart + intPageGroupSize) ;// 보여질 페이지 끝점 EX> 5페이지 까지
 
@@ -325,62 +323,60 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
          * 2. else if - 마지막 페이지 일때 : 한블럭의 게시물수 + 마지막 블럭의 게시물수를 합쳐 이전 도큐먼트ID 세팅  
          * 3. else    - 첫페이지가 아닐 때: 현재 도큐멘트 배열의 첫번째 값으로 세팅
          */
-        if(intPageGroupStart <= intPageGroupSize){//첫페이지일때 이전버튼 도큐멘트ID
-            System.out.println("☆ 1111111111111111               ------------------------------------------------------  ☆  = " + intTotalCount);
+        if(intPageGroupStart <= intPageGroupSize){//첫페이지일때 - 첫번째 도큐먼트ID를 이전버튼 도큐멘트ID로 세팅
+    
             strPrevDoc = paginationDto.getFirstDoc().split("\\|")[0];
+            
+            LOG.info("이전 게시물 아이디 세팅 Type1 : PrevDoc -> " + strPrevDoc);
         }
-        else if((intPageGroupEnd * intCountPerPage) >= intTotalCount){//마지막일때 이전버튼 도큐멘트ID
-            System.out.println("☆ 22222222222222222               ------------------------------------------------------  ☆  = " + intTotalCount);
+        else if((intPageGroupEnd * intCountPerPage) >= intTotalCount || // 1. 가는 곳이 마지막페이지 일 때
+                (intPageGroupEnd * intCountPerPage) >= (intTotalCount - (intPageGroupSize * intCountPerPage))){// 2. 마지막페이지에서 이전버튼 눌렸을 때
+            
+            /*
+            * 1. 마지막 페이지의 경우 : 마지막 페이지그룹의 게시물 갯수 와 한블록당 게시물 갯수의 합을 구하여 이전 도큐먼트ID를 호출 세팅
+            * 2. 마지막 페이지에서 이전버튼 눌렸을 경우 : 마지막 페이지그룹의 게시물 갯수 와 한블록당 게시물 갯수의 합을 구하여 이전 도큐먼트ID를 호출 세팅
+            */
+            
             Integer LastDocCnt = intTotalCount - (intPageGroupStart * intCountPerPage);//마지막 페이지그룹의 게시물 갯수
-            Integer preveDocCnt = LastDocCnt + (intCountPerPage * intPageGroupSize); //한블럭당 보여질 게시물 갯수
+            Integer prevDocCnt = LastDocCnt + (intCountPerPage * intPageGroupSize); //마지막 페이지 개시물 갯수 + 한블럭당 보여질 게시물 갯수
+            
             firebaseConfiguration.initializeFCM();
             Firestore db = FirestoreClient.getFirestore();
 
-            ApiFuture<QuerySnapshot> query = db.collection(strCollectionNm).orderBy(strOrderbyCol, Direction.ASCENDING).limitToLast(preveDocCnt).get();
+            ApiFuture<QuerySnapshot> query = db.collection(strCollectionNm).orderBy(strOrderbyCol, Direction.ASCENDING).limitToLast(prevDocCnt).get();
             List<QueryDocumentSnapshot> documents = query.get().getDocuments();
             QueryDocumentSnapshot lastDoc = documents.get(0);
 
             strPrevDoc = lastDoc.getId();
+
+            LOG.info("이전 게시물 아이디 세팅 Type2 : PrevDoc -> " + strPrevDoc);
         }
-        else{//기본 이전버튼 도큐멘트iD
-            strPrevDoc = paginationDto.getDocIdArr().split(",")[0];
-            
-            if(strCallType.equals("EACH")){//개별페이징일 경우
+        else if(strCallType.equals("EACH")){//3. 개별페이징일 경우 - 현재 DocList의 첫번째를 기준으로 이전 페이지의 이전버튼 도큐멘트ID 세팅
                 
-                System.out.println("☆  333333333333333            ------------------------------------------------------  ☆  = " + strPrevDoc);
-                firebaseConfiguration.initializeFCM();
-                Firestore db = FirestoreClient.getFirestore();
+            strPrevDoc = paginationDto.getDocIdArr().split(",")[0];
 
-                Integer callSize = intPageGroupSize*intCountPerPage;
+            firebaseConfiguration.initializeFCM();
+            Firestore db = FirestoreClient.getFirestore();
 
-                ApiFuture<DocumentSnapshot> future = db.collection(strCollectionNm).document(strPrevDoc).get();
-                DocumentSnapshot snapshot = future.get(30, TimeUnit.SECONDS);
+            Integer callSize = intPageGroupSize*intCountPerPage;
 
-                ApiFuture<QuerySnapshot> query = db.collection(strCollectionNm).orderBy(strOrderbyCol, Direction.ASCENDING).endBefore(snapshot).limitToLast(callSize).get();
-                List<QueryDocumentSnapshot> documents = query.get().getDocuments();
+            ApiFuture<DocumentSnapshot> future = db.collection(strCollectionNm).document(strPrevDoc).get();
+            DocumentSnapshot snapshot = future.get(30, TimeUnit.SECONDS);
 
-                for (QueryDocumentSnapshot document : documents) {
+            ApiFuture<QuerySnapshot> query = db.collection(strCollectionNm).orderBy(strOrderbyCol, Direction.ASCENDING).endBefore(snapshot).limitToLast(callSize).get();
+            List<QueryDocumentSnapshot> documents = query.get().getDocuments();
 
-                    String docId = document.getId();
-                    System.out.println(docId);
-                    //docIdList.add(docId);
-                }
+            QueryDocumentSnapshot lastDoc = documents.get(0);
 
+            strPrevDoc = lastDoc.getId();
 
-                //QueryDocumentSnapshot lastDoc = documents.get(documents.size()-1);
-                QueryDocumentSnapshot lastDoc = documents.get(0);
+            LOG.info("이전 게시물 아이디 세팅 Type3 : PrevDoc -> " + strPrevDoc);
+        }
+        else{//4. 기본 이전페이지 - 갱신전 DocList의 첫 도큐먼트ID 이전페이지로 설정
+            
+            strPrevDoc = paginationDto.getDocIdArr().split(",")[0];
 
-                strPrevDoc = lastDoc.getId();
-            }
-            else{//이전 버튼을 눌렸을 경우
-                /*
-                 * 원래() - 기존독 리스트의 첫 번째가 이전페이지
-                 * 마지막 페이지에서 이전 페이지를 눌렸을 경우 이전 페이지는 마지막 페이지의 이전페이지가 된다. (16페이지였을때의 이전페이지)
-                 * 거기서 이전페이
-                 */
-                System.out.println("☆  444444444444444444            ------------------------------------------------------  ☆  = " + strPrevDoc);
-                strPrevDoc = paginationDto.getDocIdArr().split(",")[0];
-            }
+            LOG.info("이전 게시물 아이디 세팅 Type4 : PrevDoc -> " + strPrevDoc);
         }
 
         return strPrevDoc;
@@ -396,7 +392,7 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
     @Override
     public String getNextDoc(PaginationDto paginationDto) throws Exception {
 
-        long reqTime = Util.durationTime ("start", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", 0, "Proceeding ::: " );
+        long reqTime = Util.durationTime ("start", "CLOUD / < GET PAGING NEXT DOC - SELECT > : ", 0, "Proceeding ::: " );
         
         String strNextDocId = "";
         
@@ -404,8 +400,7 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
         String strDocIdArrLast = docIdArr[docIdArr.length-1].trim();//배열의 자미가 도큐먼트ID를 다음페이지의 기준으로 사용
         String strCollectionNm = paginationDto.getCollectionNm();//도큐먼트를 가져올 컬렉션 명
         String strOrderbyCol = paginationDto.getOrderbyCol();//도큐먼트를 가져올 컬렉션의 정렬 컬럼
-        System.out.println("★★★★★★★★★★★★★★★★★★★");
-        System.out.println(paginationDto.getDocIdArr());
+
         LOG.info("다음 게시물 아이디 세팅 : NextDoc -> " + strDocIdArrLast);
 
         try {
@@ -425,10 +420,10 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
                 strNextDocId = document.getId();
             }
 
-            Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Complete ::: " );
+            Util.durationTime ("end", "CLOUD / < GET PAGING NEXT DOC - SELECT > : ", reqTime, "Complete ::: " );
         }
         catch (Exception e) {
-			Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Fail ::: " );
+			Util.durationTime ("end", "CLOUD / < GET PAGING NEXT DOC - SELECT > : ", reqTime, "Fail ::: " );
             e.printStackTrace();
         }
 
@@ -445,7 +440,7 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
     @Override
     public String getLastDoc(PaginationDto paginationDto) throws Exception {
 
-        long reqTime = Util.durationTime ("start", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", 0, "Proceeding ::: " );
+        long reqTime = Util.durationTime ("start", "CLOUD / < GET PAGING LAST DOC - SELECT > : ", 0, "Proceeding ::: " );
 
         String strLastDocId = "";//마지막 도큐먼트 ID
         String strCollectionNm = paginationDto.getCollectionNm();//도큐먼트를 가져올 컬렉션 명
@@ -466,15 +461,16 @@ public class PaginationFirbaseServiceImpl implements PaginationFirebaseService{
 
             strLastDocId = lastDoc.getId();
 
-            Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Complete ::: " );
+            Util.durationTime ("end", "CLOUD / < GET PAGING LAST DOC - SELECT > : ", reqTime, "Complete ::: " );
         }
         catch (Exception e) {
-			Util.durationTime ("end", "CLOUD / < SET DELETE GEOMETRY BOARD - DELETE > : ", reqTime, "Fail ::: " );
+			Util.durationTime ("end", "CLOUD / < GET PAGING LAST DOC - SELECT > : ", reqTime, "Fail ::: " );
             e.printStackTrace();
         }
 
         return strLastDocId;
     }
+
 
 
     /**
