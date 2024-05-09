d619d940274b7aa05dfbf90461e0b22e7a0ee3b5
cho younghyun <5869jyh@hanmail.net>
2024-05-09 18:02:21 +0900

    Work_Toast UI작업

--- vue/src/commonLayout/ToastLayout.vue
+++ vue/src/commonLayout/ToastLayout.vue
@@ -17,31 +17,19 @@
 <!-- [개별 템플릿 (뷰) 설정 실시] -->
 <template>
     <div>
-        <b-button @click="toast('b-toaster-top-right')" class="mb-2">b-toaster-top-right</b-button>
-        <b-button @click="toast('b-toaster-top-left')" class="mb-2">b-toaster-top-left</b-button>
-        <b-button @click="toast('b-toaster-top-center')" class="mb-2">b-toaster-top-center</b-button>
-        <b-button @click="toast('b-toaster-top-full')" class="mb-2">b-toaster-top-full</b-button>
-        <b-button @click="toast('b-toaster-bottom-right', true)" class="mb-2">b-toaster-bottom-right</b-button>
-        <b-button @click="toast('b-toaster-bottom-left', true)" class="mb-2">b-toaster-bottom-left</b-button>
-        <b-button @click="toast('b-toaster-bottom-center', true)" class="mb-2">b-toaster-bottom-center</b-button>
-        <b-button @click="toast('b-toaster-bottom-full', true)" class="mb-2">b-toaster-bottom-full</b-button>
+        <b-button @click="showToast" class="mb-2">b-toaster-bottom-full</b-button>
 
-        <BToast />
+        <b-toast v-model:show="showToast" title="Toast Title"> This is a toast message. </b-toast>
     </div>
 </template>
 
 <!-- [개별 스크립트 설정 실시] -->
 <script>
-import { BToast } from 'bootstrap-vue-3';
-
 export default {
     data() {
         return {
             counter: 0,
         };
-    },
-    components: {
-        BToast,
     },
     props: ['parsingList'], // myArray prop을 정의하여 받는다
     methods: {
